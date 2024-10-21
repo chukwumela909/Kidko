018318366993ff32172f0feff32bca1e6d0719f0
Amirize Wisdom <97132123+chukwumela909@users.noreply.github.com>
2024-10-21 12:48:53 +0100

    ghg

--- script/vote.js
+++ script/vote.js
@@ -9,9 +9,44 @@ function handlePreloader() {
 handlePreloader()
 
 
-const vote_btn = document.getElementById("vote")
 
 
-vote_btn.addEventListener('click' , function(){
-    console.log("clicked")
+const vote_btns = document.querySelectorAll(".vote")
+
+const vote_dialog = document.querySelector(".fixed-dialog")
+const voter_name = document.querySelector("#voter_name")
+const voter_email = document.querySelector("#voter_email")
+const voter_votes = document.querySelector("#voter_votes")
+const vote_dialog_proceed = document.querySelector(".submit-btn")
+const vote_dialog_cancel = document.querySelector(".cancel-btn")
+
+
+
+vote_btns.forEach(button => {
+    button.addEventListener('click', () => {
+        vote_dialog.style.display = "block"
+        vote_dialog_proceed.addEventListener('click', () => {
+            if (voter_name.value.trim() === "" || voter_email.value.trim() === "" || voter_votes.value.trim() === "") {
+               return Swal.fire({
+                    title: "Error",
+                    text: 'Input fields cannot be left blank',
+                    icon: "error"
+                });
+            } else {
+                localStorage.setItem('voter_name', voter_name.value);
+                localStorage.setItem('voter_email', voter_email.value);
+                localStorage.setItem('voter_votes', voter_votes.value);
+                vote_dialog.style.display = "none";
+            }
+        });
+        
+    });
+});
+
+
+
+vote_dialog_cancel.addEventListener('click', () => {
+    vote_dialog.style.display = "none"
 })
+
+
